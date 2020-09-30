912759eb12568b058cfcf8c1cb33256590f743cc
Селляви Админ <sellaviadmin@MacBook-Pro-Sellavi.local>
2020-09-30 15:50:21 +0300

    Added some features with bindings

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
         ]
     }
 
-    switchTimeHandler = () => {
+    switchTimeHandler = (newAge) => {
         this.setState({
             person: [
-                { name: 'Ruslan in future', age: '25', hobby: 'and my hobby is Racing' },
+                { name: 'Ruslan in future', age: newAge, hobby: 'and my hobby is Racing' },
                 { name: 'Zakir in future', age: '30', hobby: 'and my hobby is Dog`s Teaching' },
                 { name: 'Timur in future', age: '35', hobby: 'and my hobby is fapping on BMW :D' },
             ]
@@ -25,10 +25,19 @@ class App extends Component {
     return (
         <div className=".block">
           <h1>Our Team</h1>
-            <Person name={this.state.person[0].name} age={this.state.person[0].age}> {this.state.person[0].hobby} </Person>
-            <Person name={this.state.person[1].name} age={this.state.person[1].age}> {this.state.person[1].hobby} </Person>
-            <Person name={this.state.person[2].name} age={this.state.person[2].age}> {this.state.person[2].hobby} </Person>
-            <button onClick={this.switchTimeHandler}>Go to the future</button>
+            <Person
+                name={this.state.person[0].name}
+                age={this.state.person[0].age}> {this.state.person[0].hobby} </Person>
+            <Person
+                name={this.state.person[1].name}
+                age={this.state.person[1].age}
+                click={this.switchTimeHandler.bind(this, '23')}> {this.state.person[1].hobby} </Person>
+            <Person
+                name={this.state.person[2].name}
+                age={this.state.person[2].age}> {this.state.person[2].hobby} </Person>
+            {/*<label>New Age for Ruslan:</label>
+            <input className={"nAge"} type={"number"} />*/}
+            <button onClick={this.switchTimeHandler.bind(this, '29')}>Go to the future</button>
         </div>
     )
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <span>My name is {props.name} and I am {props.age} years old</span>
+            <span onClick={props.click}>My name is {props.name} and I am {props.age} years old</span>
             <b>{props.children}</b>
         </div>
     )
