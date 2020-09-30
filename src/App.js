1061389784611b1f862fe3895457431a01e5fb53
Селляви Админ <sellaviadmin@MacBook-Pro-Sellavi.local>
2020-09-30 18:37:00 +0300

    Updated toggle

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
             { name: 'Ruslan', age: '22', hobby: 'sport' },
             { name: 'Zakir', age: '23', hobby: 'photography' },
             { name: 'Timur', age: '24', hobby: 'fapping on VAG' },
-        ]
+        ],
+        showPersons: false
     }
 
+    // Look in future
     switchTimeHandler = (newAge) => {
         this.setState({
             person: [
@@ -21,34 +23,49 @@ class App extends Component {
         });
     }
 
-    switchHobbyHandler = (e) => {
+    // Edit hobby
+    switchHobbyHandler = ( event ) => {
         this.setState({
             person: [
-                { name: 'Ruslan in future', age: '22', hobby: e.target.value },
+                { name: 'Ruslan in future', age: '22', hobby: event.target.value },
                 { name: 'Zakir in future', age: '30', hobby: 'Dog`s Teaching' },
                 { name: 'Timur in future', age: '35', hobby: 'fapping on BMW :D' },
             ]
         });
     }
 
+    // Show/toggle persons
+    togglePersonHandler = () => {
+        const doesShow = this.state.showPersons
+        this.setState({ showPersons: !doesShow })
+    }
+
   render() {
+        let persons = null;
+        if( this.state.showPersons ) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.person[0].name}
+                        age={this.state.person[0].age}
+                        hobby={this.state.person[0].hobby} changed={this.switchHobbyHandler}> My hobby is: {this.state.person[0].hobby} </Person>
+                    <Person
+                        name={this.state.person[1].name}
+                        age={this.state.person[1].age}
+                        hobby={this.state.person[1].hobby}
+                        click={this.switchTimeHandler.bind(this, '23')}> My hobby is:  {this.state.person[1].hobby} </Person>
+                    <Person
+                        name={this.state.person[2].name}
+                        age={this.state.person[2].age}
+                        hobby={this.state.person[2].hobby} > My hobby is:  {this.state.person[2].hobby} </Person>
+                </div>
+            )
+        }
     return (
         <div className=".block">
           <h1>Our Team</h1>
-            <Person
-                name={this.state.person[0].name}
-                age={this.state.person[0].age}
-                hobby={this.state.person[0].hobby} changed={this.switchHobbyHandler}>My hobby is: {this.state.person[0].hobby} </Person>
-            <Person
-                name={this.state.person[1].name}
-                age={this.state.person[1].age}
-                hobby={this.state.person[1].hobby}
-                click={this.switchTimeHandler.bind(this, '23')}> My hobby is:  {this.state.person[1].hobby} </Person>
-            <Person
-                name={this.state.person[2].name}
-                age={this.state.person[2].age}
-                hobby={this.state.person[2].hobby} > My hobby is:  {this.state.person[2].hobby} </Person>
-            <button onClick={this.switchTimeHandler.bind(this, '29')}>Go to the future</button>
+            <button onClick={this.togglePersonHandler}>Toggle</button>
+            { persons }
         </div>
     )
   }
