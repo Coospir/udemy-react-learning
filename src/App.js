ed63187fcd55effe6b4d63bd4f47009c94920422
Селляви Админ <sellaviadmin@MacBook-Pro-Sellavi.local>
2020-09-30 16:16:16 +0300

    Adding inputs for editing hobbies

--- src/App.js
+++ src/App.js
@@ -5,18 +5,28 @@ import './App.css';
 class App extends Component {
     state = {
         person: [
-            { name: 'Ruslan', age: '22', hobby: 'and my hobby is sport' },
-            { name: 'Zakir', age: '23', hobby: 'and my hobby is photography' },
-            { name: 'Timur', age: '24', hobby: 'and my hobby is fapping on VAG' },
+            { name: 'Ruslan', age: '22', hobby: 'sport' },
+            { name: 'Zakir', age: '23', hobby: 'photography' },
+            { name: 'Timur', age: '24', hobby: 'fapping on VAG' },
         ]
     }
 
     switchTimeHandler = (newAge) => {
         this.setState({
             person: [
-                { name: 'Ruslan in future', age: newAge, hobby: 'and my hobby is Racing' },
-                { name: 'Zakir in future', age: '30', hobby: 'and my hobby is Dog`s Teaching' },
-                { name: 'Timur in future', age: '35', hobby: 'and my hobby is fapping on BMW :D' },
+                { name: 'Ruslan in future', age: newAge, hobby: 'Racing' },
+                { name: 'Zakir in future', age: '30', hobby: 'Dog`s Teaching' },
+                { name: 'Timur in future', age: '35', hobby: 'fapping on BMW :D' },
+            ]
+        });
+    }
+
+    switchHobbyHandler = (e) => {
+        this.setState({
+            person: [
+                { name: 'Ruslan in future', age: '22', hobby: e.target.value },
+                { name: 'Zakir in future', age: '30', hobby: 'Dog`s Teaching' },
+                { name: 'Timur in future', age: '35', hobby: 'fapping on BMW :D' },
             ]
         });
     }
@@ -27,16 +37,17 @@ class App extends Component {
           <h1>Our Team</h1>
             <Person
                 name={this.state.person[0].name}
-                age={this.state.person[0].age}> {this.state.person[0].hobby} </Person>
+                age={this.state.person[0].age}
+                hobby={this.state.person[0].hobby} changed={this.switchHobbyHandler}>My hobby is: {this.state.person[0].hobby} </Person>
             <Person
                 name={this.state.person[1].name}
                 age={this.state.person[1].age}
-                click={this.switchTimeHandler.bind(this, '23')}> {this.state.person[1].hobby} </Person>
+                hobby={this.state.person[1].hobby}
+                click={this.switchTimeHandler.bind(this, '23')}> My hobby is:  {this.state.person[1].hobby} </Person>
             <Person
                 name={this.state.person[2].name}
-                age={this.state.person[2].age}> {this.state.person[2].hobby} </Person>
-            {/*<label>New Age for Ruslan:</label>
-            <input className={"nAge"} type={"number"} />*/}
+                age={this.state.person[2].age}
+                hobby={this.state.person[2].hobby} > My hobby is:  {this.state.person[2].hobby} </Person>
             <button onClick={this.switchTimeHandler.bind(this, '29')}>Go to the future</button>
         </div>
     )
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,8 @@ const person = (props) => {
         <div>
             <span onClick={props.click}>My name is {props.name} and I am {props.age} years old</span>
             <b>{props.children}</b>
+            <label>Change hobby >>>  </label>
+            <input type={"text"} onChange={props.changed} value={props.hobby}/>
         </div>
     )
 }
