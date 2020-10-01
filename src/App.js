2d85caf1e7f2f3324778cce972f521c645414490
Селляви Админ <sellaviadmin@MacBook-Pro-Sellavi.local>
2020-10-01 14:28:47 +0300

    Dynamic rendering list of Persons

--- src/App.js
+++ src/App.js
@@ -4,34 +4,35 @@ import './App.css';
 
 class App extends Component {
     state = {
-        person: [
-            { name: 'Ruslan', age: '22', hobby: 'sport' },
-            { name: 'Zakir', age: '23', hobby: 'photography' },
-            { name: 'Timur', age: '24', hobby: 'fapping on VAG' },
+        persons: [
+            { id: '1', name: 'Ruslan', age: '22', hobby: 'sport' },
+            { id: '2', name: 'Zakir', age: '23', hobby: 'photography' },
+            { id: '3', name: 'Timur', age: '24', hobby: 'fapping on VAG' },
         ],
         showPersons: false
     }
 
-    // Look in future
-    switchTimeHandler = (newAge) => {
-        this.setState({
-            person: [
-                { name: 'Ruslan in future', age: newAge, hobby: 'Racing' },
-                { name: 'Zakir in future', age: '30', hobby: 'Dog`s Teaching' },
-                { name: 'Timur in future', age: '35', hobby: 'fapping on BMW :D' },
-            ]
-        });
+    // Edit hobby
+    switchHobbyHandler = ( event, id ) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        })
+
+        const person = {
+            ...this.state.persons[personIndex]
+        }
+        person.hobby = event.target.value;
+
+        const persons = [...this.state.persons]
+        persons[personIndex] = person
+
+        this.setState({ persons: persons });
     }
 
-    // Edit hobby
-    switchHobbyHandler = ( event ) => {
-        this.setState({
-            person: [
-                { name: 'Ruslan in future', age: '22', hobby: event.target.value },
-                { name: 'Zakir in future', age: '30', hobby: 'Dog`s Teaching' },
-                { name: 'Timur in future', age: '35', hobby: 'fapping on BMW :D' },
-            ]
-        });
+    deletePersonHandler = (personIndex) => {
+        const persons = [...this.state.persons]
+        persons.splice(personIndex, 1)
+        this.setState({persons: persons})
     }
 
     // Show/toggle persons
@@ -45,26 +46,22 @@ class App extends Component {
         if( this.state.showPersons ) {
             persons = (
                 <div>
-                    <Person
-                        name={this.state.person[0].name}
-                        age={this.state.person[0].age}
-                        hobby={this.state.person[0].hobby} changed={this.switchHobbyHandler}> My hobby is: {this.state.person[0].hobby} </Person>
-                    <Person
-                        name={this.state.person[1].name}
-                        age={this.state.person[1].age}
-                        hobby={this.state.person[1].hobby}
-                        click={this.switchTimeHandler.bind(this, '23')}> My hobby is:  {this.state.person[1].hobby} </Person>
-                    <Person
-                        name={this.state.person[2].name}
-                        age={this.state.person[2].age}
-                        hobby={this.state.person[2].hobby} > My hobby is:  {this.state.person[2].hobby} </Person>
+                    {this.state.persons.map( ( person, index ) => {
+                        return <Person
+                            name={person.name}
+                            age={person.age}
+                            hobby={person.hobby}
+                            click={() => this.deletePersonHandler(index)}
+                            key={person.id}
+                            changed={(event) => this.switchHobbyHandler(event, person.id)}/>
+                    })}
                 </div>
             )
         }
     return (
         <div className=".block">
           <h1>Our Team</h1>
-            <button onClick={this.togglePersonHandler}>Toggle</button>
+            <button onClick={this.togglePersonHandler}>Show</button>
             { persons }
         </div>
     )
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,9 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <span onClick={props.click}>My name is {props.name} and I am {props.age} years old</span>
-            <b>{props.children}</b>
-            <label>Change hobby >>>  </label>
+            <span onClick={props.click}>My name is {props.name} and I am {props.age} years old.</span>
+            <b>My hobby is {props.hobby}</b>
+            <label> Change hobby >>>  </label>
             <input type={"text"} onChange={props.changed} value={props.hobby}/>
         </div>
     )
